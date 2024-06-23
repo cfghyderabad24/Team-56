d8927672bdbaec2386b145fd2d1b14abb4d817c3
AsadiSaiBhavyaReddy <saibhavyaasadi@gmail.com>
2024-06-23 05:32:51 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const client = new MongoClient('mongodb+srv://team-56:team-56@team-56.sgr8gj9.mongodb.net/?retryWrites=true&w=majority&appName=team-56');
+const client = new MongoClient('mongodb+srv://admin10:admin10@admin10.6lafpmt.mongodb.net/?retryWrites=true&w=majority&appName=admin10');
 client.connect();
 
 const db = client.db("skill");
@@ -15,6 +15,7 @@ const col = db.collection("user");
 const col2 = db.collection("student");
 const col3 = db.collection("view");
 const col4 = db.collection("faculty");
+const col5 = db.collection("vol");
 
 app.get('/home', (req, res) => {
     res.send("It is a Home Page - New Page - New 2 Page");
@@ -29,7 +30,7 @@ app.post('/insert', async (req, res) => {
 
 app.post('/check', async (req, res) => {
     console.log(req.body);
-    var result = await col.findOne({"name": req.body.un});
+    var result = await col.findOne({ "name": req.body.un });
     if (result != null) {
         if (await bcrypt.compare(req.body.pw, result.password)) {
             res.send(result);
@@ -62,6 +63,12 @@ app.post('/ent', (req, res) => {
 app.post('/fac', (req, res) => {
     console.log(req.body);
     col4.insertOne(req.body);
+    res.send("Successfully Inserted");
+});
+
+app.post('/vol', (req, res) => {
+    console.log(req.body);
+    col5.insertOne(req.body);
     res.send("Successfully Inserted");
 });
 
@@ -105,7 +112,29 @@ app.put('/fac', async (req, res) => {
     };
     await col4.updateOne({ sid: req.body.sid }, doc);
     res.send("updated successfully");
-})
+});
+
+app.put('/vol', async (req, res) => {
+    console.log(req.body);
+    var doc = {
+        $set: {
+            parentName: req.body.parentName,
+            gender: req.body.gender,
+            age: req.body.age,
+            mobile: req.body.mobile,
+            email: req.body.email,
+            studentId: req.body.studentId,
+            studentName: req.body.studentName,
+            department: req.body.department,
+            purpose: req.body.purpose,
+            date: req.body.date,
+            time: req.body.time,
+            fileName: req.body.fileName
+        }
+    };
+    await col5.updateOne({ studentId: req.body.studentId }, doc);
+    res.send("updated successfully");
+});
 
 app.get('/display', async (req, res) => {
     var result = await col2.find().toArray();
@@ -131,12 +160,25 @@ app.delete('/del', async (req, res) => {
 
 app.get('/d', async (req, res) => {
     var result = await col4.find().toArray();
+    console.log('in d', result);
     res.send(result);
+
 });
 
 app.delete('/de', async (req, res) => {
     console.log(req.query);
     await col4.deleteOne({ sid: req.query.id });
+    res.send("deleted");
+});
+
+app.get('/displ', async (req, res) => {
+    var result = await col5.find().toArray();
+    res.send(result);
+});
+
+app.delete('/dlet', async (req, res) => {
+    console.log(req.query);
+    await col5.deleteOne({ studentId: req.query.id });
     res.send("deleted");
 });
 
